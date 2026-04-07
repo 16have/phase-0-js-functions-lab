4a9273db17de8aa2d17542f2e8bc165fed9cbd9b
kelvin <joer88544@gmail.com>
2026-04-07 12:54:43 +0300

    submition

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+// Function to calculate tax
+function calculateTax(amount) {
+    return amount * (10 / 100);
+}
+function convertToUpperCase(str) {    return str.toUpperCase();
+}   
+// Function to find the maximum of two numbers
+function findMaximum(num1, num2) {
+    return num1 > num2 ? num1 : num2;
+}
+// Function to check if a string is a palindrome
+function isPalindrome(str) {
+    const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    return cleanedStr === cleanedStr.split('').reverse().join('');
+}
+// Function to calculate discounted price
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+    return originalPrice - (originalPrice * (discountPercentage / 100));
+}
 
 
 
